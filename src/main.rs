9fa56905701e5c8c862e5a62d5ef911260a2021d
jdumont0201 <32376919+jdumont0201@users.noreply.github.com>
2018-01-28 10:49:29 +0100

    f

--- src/main.rs
+++ src/main.rs
@@ -72,8 +72,8 @@ fn main() {
     chain.link_after(ResponseTime);
     chain.link_after(Custom404);
     //chain.link(Write::<HitCounter>::both(0));
-    static http_port: i32 = 3020;
-    if let Ok(server) = Iron::new(chain).http("localhost:8081") {
+    static http_port: i32 = 8081;
+    if let Ok(server) = Iron::new(chain).http("127.0.0.1:8081") {
         println!("HTTP server listening on {}", http_port);
     } else {
         println!("HTTP server could not connect on {}", http_port);
