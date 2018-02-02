0e98d879156dec5d5a4bc1ba211d6897b739c9aa
jdumont0201 <32376919+jdumont0201@users.noreply.github.com>
2018-02-02 17:34:12 +0100

    separate price bidask

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ extern crate router;
 extern crate chrono;
 extern crate serde;
 extern crate serde_json;
-
 #[macro_use]
 extern crate serde_derive;
 extern crate reqwest;
@@ -15,7 +14,6 @@ use iron::{BeforeMiddleware, AfterMiddleware, typemap};
 use time::precise_time_ns;
 use std::thread;
 use iron::status;
-
 use std::fs::File;
 use std::io::prelude::*;
 use router::{Router, NoRoute};
@@ -23,9 +21,19 @@ use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
 
 struct ResponseTime;
+
 impl typemap::Key for ResponseTime { type Value = u64; }
+
 struct Custom404;
-type BidaskRegistry = Arc<Mutex<Option<HashMap<String, String>>>>;
+
+pub struct Data {
+    bid: Option<String>,
+    ask: Option<String>,
+    last: Option<String>,
+}
+
+type BidaskRegistry = Arc<Mutex<Option<HashMap<String, HashMap<String, Data>>>>>;
+type BidaskTextRegistry = Arc<Mutex<Option<HashMap<String, String>>>>;
 
 impl BeforeMiddleware for ResponseTime {
     fn before(&self, req: &mut Request) -> IronResult<()> {
@@ -37,7 +45,7 @@ impl BeforeMiddleware for ResponseTime {
 impl AfterMiddleware for ResponseTime {
     fn after(&self, req: &mut Request, res: Response) -> IronResult<Response> {
         let delta = precise_time_ns() - *req.extensions.get::<ResponseTime>().unwrap();
-        println!("{:?} Request took: {} ms", *req, (delta as f64) / 1000000.0);
+        println!("{} Request took: {} ms", req.url, (delta as f64) / 1000000.0);
         Ok(res)
     }
 }
@@ -56,23 +64,29 @@ impl AfterMiddleware for Custom404 {
 
 fn main() {
     let mut children = vec![];
-    let mut ae: HashMap<String, String> = HashMap::new();
-    ae.insert("binance".to_string(), "".to_string());
-    ae.insert("hitbtc".to_string(), "".to_string());
+
+    let mut ae: HashMap<String, HashMap<String, Data>> = HashMap::new();
+    ae.insert("binance".to_string(), HashMap::new());
+    ae.insert("hitbtc".to_string(), HashMap::new());
     let mut bidask: BidaskRegistry = Arc::new(Mutex::new(Some(ae)));
-    //let mut TICKER: HashMap<String,String> = HashMap::new();
+
+    let mut aet: HashMap<String, String> = HashMap::new();
+    aet.insert("binance".to_string(), "".to_string());
+    aet.insert("hitbtc".to_string(), "".to_string());
+    let mut bidasktxt: BidaskTextRegistry = Arc::new(Mutex::new(Some(aet)));
+
     let bidask2 = bidask.clone();
+    let bidaskt2 = bidasktxt.clone();
     children.push(thread::spawn(move || {
 
         //HTTP
         println!("Coinamics Server HTTP");
         let mut router = Router::new();
         router.get("/", handler_simple, "index");
-        router.get("/:query", handler_query_bars, "query");
-        router.get("/favicon.ico", handler_favicon, "favicon");
-        router.get("/:broker/:pair/:interval/:limit", handler_query_bars, "data");
 
-        let bidask3 = bidask2.clone();
+        router.get("/favicon.ico", handler_favicon, "favicon");
+
+        let bidask3 = bidaskt2.clone();
         router.get("/public/:broker/bidask", move |request: &mut Request| get_bidask(request, &bidask3), "ticker");
         let mut chain = Chain::new(router);
         chain.link_before(ResponseTime);
@@ -91,11 +105,14 @@ fn main() {
     children.push(thread::spawn(move || {
         println!("update data thread");
         let mut sched = job_scheduler::JobScheduler::new();
-        //let mut tick=ticker.clone();
         sched.add(job_scheduler::Job::new("1/10 * * * * *".parse().unwrap(), || {
-            println!("refresh bidask");
-            refresh_bidask("binance".to_string(),&bidask);
-            refresh_bidask("hitbtc".to_string(),&bidask);
+            println!("---------------- refresh --------------------");
+            refresh_bidask("binance".to_string(), &bidask, &bidasktxt);
+            refresh_bidask("hitbtc".to_string(),&bidask,&bidasktxt);
+
+            thread::sleep(std::time::Duration::new(2, 0));
+            refresh_price("binance".to_string(), &bidask, &bidasktxt);
+
         }));
         loop {
             sched.tick();
@@ -107,24 +124,100 @@ fn main() {
     }
 }
 
-fn refresh_bidask(broker: String, bidask:&BidaskRegistry) {
+fn refresh_bidask(broker: String, mut bidask: &BidaskRegistry, bidaskt: &BidaskTextRegistry) {
     if let Ok(mut opt) = bidask.lock() {
         if let Some(ref mut hm) = *opt { //open option
-            println!("refresh bidask {}", broker);
-            let mut val: Option<&mut String> = hm.get_mut(&broker);
+            let mut val: Option<&mut HashMap<String, Data>> = hm.get_mut(&broker);
             if let Some(mut vv) = val {
-                let text = Universal::fetch_bidask(broker);
-
-                *vv = text;
+                let fetched = Universal::fetch_bidask(&broker);
+                update_data(&broker, vv, &fetched);
+                let text = hmToText(vv);
+                update_bidasktext(&broker, text, bidaskt);
+                //*vv=hm;
             }
         } else {}
     }
 }
 
-fn get_bidask(req: &mut Request, ticker: &BidaskRegistry) -> IronResult<Response> {
-    println!("bidask");
+fn refresh_price(broker: String, mut bidask: &BidaskRegistry, bidaskt: &BidaskTextRegistry) {
+    if let Ok(mut opt) = bidask.lock() {
+        if let Some(ref mut hm) = *opt { //open option
+            let mut val: Option<&mut HashMap<String, Data>> = hm.get_mut(&broker);
+            if let Some(mut vv) = val {
+                let fetched = Universal::fetch_price(&broker);
+                update_data(&broker, vv, &fetched);
+                let text = hmToText(vv);
+                update_bidasktext(&broker, text, bidaskt);
+                //*vv=hm;
+            } else {
+            }
+        } else {}
+    }
+}
+
+//updates the arc mutex
+fn update_data(broker: &String, mut persistent: &mut HashMap<String, Data>, fetched: &HashMap<String, Data>) {
+    for (symbol, ref data) in fetched.iter() {
+        let mut insert: bool = false;
+        match persistent.get_mut(symbol) {
+            Some(ref mut d) => {
+                if data.last.is_some() {  d.last = data.last.clone(); }
+                if data.ask.is_some() {  d.ask = data.ask.clone(); }
+                if data.bid.is_some() { d.bid = data.bid.clone(); }
+            }
+            None => {
+                insert = true;
+            }
+        }
+        if insert {
+            persistent.insert(symbol.to_string(), Data { last: data.last.clone(), ask: data.ask.clone(), bid: data.bid.clone() });
+        }
+
+    }
+}
+
+fn update_bidasktext(broker: &String, text: String, bidaskt: &BidaskTextRegistry) {
+    if let Ok(mut opt) = bidaskt.lock() {
+        if let Some(ref mut hm) = *opt { //open option
+            let mut val: Option<&mut String> = hm.get_mut(broker);
+            if let Some(mut vv) = val {
+                *vv = text;
+            }
+        }
+    }
+}
+
+fn hmToText(hm: &HashMap<String, Data>) -> String {
+    let mut st = "{".to_string();
+    let mut first = true;
+    for (symbol, data) in hm.iter() {
+        let bid: String;
+        let ask: String;
+        let last: String;
+        match data.bid {
+            Some(ref b) => { bid = b.to_string() }
+            None => bid = "".to_string()
+        }
+        match data.ask {
+            Some(ref b) => { ask = b.to_string() }
+            None => ask = "".to_string()
+        }
+        match data.last {
+            Some(ref b) => { last = b.to_string() }
+            None => last = "".to_string()
+        }
+        if first {
+            st = format!("{}\"{}\":{{\"bid\":{},\"ask\":{},\"last\":{}}}", st, symbol, bid, ask, last);
+        } else {
+            st = format!("{},\"{}\":{{\"bid\":{},\"ask\":{},\"last\":{}}}", st, symbol, bid, ask, last);
+        }
+        first = false;
+    }
+    format!("{}}}", st)
+}
+
+fn get_bidask(req: &mut Request, ticker: &BidaskTextRegistry) -> IronResult<Response> {
     let ref broker: &str = req.extensions.get::<Router>().unwrap().find("broker").unwrap_or("/");
-    println!("bidask {}", broker);
     let key: String = broker.to_string();
     let mut val: String = "".to_string();
     if let Ok(mut opt) = ticker.lock() {
@@ -150,6 +243,12 @@ struct binance_bidask {
     askQty: String,
 }
 
+#[derive(Serialize, Deserialize)]
+struct binance_price {
+    symbol: String,
+    price: String,
+}
+
 
 #[derive(Serialize, Deserialize)]
 struct hitbtc_bidask {
@@ -168,57 +267,76 @@ struct hitbtc_bidask {
 
 mod Universal {
     use reqwest;
+    use std::collections::HashMap;
+    use Data;
 
-    fn format(task: String, broker: String, text: String) -> String {
-        let mut r = "{".to_string();
-        let mut first = true;
+    fn getGeneric_hashmap(task: String, broker: String, text: String) -> HashMap<String, Data> {
+        let mut r = HashMap::new();
         if task == "bidask" {
             if broker == "binance" {
                 let bs: Vec<super::binance_bidask> = super::serde_json::from_str(&text).unwrap();
                 for row in bs {
-                    if first {
-                        r = format!("{}\"{}\":{{\"bid\":{},\"ask\":{}}}", r, row.symbol, row.bidPrice, row.askPrice);
-                    } else {
-                        r = format!("{},\"{}\":{{\"bid\":{},\"ask\":{}}}", r, row.symbol, row.bidPrice, row.askPrice);
-                    }
-                    first = false;
+                    r.insert(row.symbol, Data { bid: Some(row.bidPrice), ask: Some(row.askPrice), last: None });
                 }
             } else if broker == "hitbtc" {
-                println!("format {} {} ",broker,text);
+                //println!("format {} {} ",broker,text);
                 let bs: Vec<super::hitbtc_bidask> = super::serde_json::from_str(&text).unwrap();
                 for row in bs {
-                    let bid;
-                    match row.bid {
-                        Some(b)=>{ bid=b;},
-                        None => {bid="".to_string()}
-                    };
-                    if first {
-                        r = format!("{}\"{}\":{{\"bid\":{},\"ask\":{}}}", r, row.symbol, bid, row.ask);
-                    } else {
-                        r = format!("{},\"{}\":{{\"bid\":{},\"ask\":{},\"p\":{}}}", r, row.symbol, bid, row.ask,row.last);
-                    }
-                    first = false;
+
+                    r.insert(row.symbol, Data { bid: row.bid, ask: Some(row.ask), last: Some(row.last) });
+                }
+            }
+        } else if task == "price" {
+            if broker == "binance" {
+                let bs: Vec<super::binance_price> = super::serde_json::from_str(&text).unwrap();
+                for row in bs {
+                    r.insert(row.symbol, Data { bid: None, ask: None, last: Some(row.price) });
                 }
             }
         }
-        format!("{}}}", r)
+        r
     }
 
-    pub fn fetch_bidask(broker: String) -> String {
-        let url = get_url("bidask".to_string(), &broker);
-        println!("url {}",url);
-        let mut result: String = "".to_string();
+    pub fn fetch_bidask(broker: &String) -> HashMap<String, Data> {
+        let url = get_url("bidask".to_string(), broker);
+        let mut result: HashMap<String, Data>;
         if let Ok(mut res) = reqwest::get(&url) {
             let getres = match res.text() {
                 Ok(val) => {
-                    let v = format("bidask".to_string(), broker, val);
+                    let v = getGeneric_hashmap("bidask".to_string(), broker.to_string(), val);
                     result = v;
                 }
                 Err(err) => {
-                    println!("[GET_TICKER] err");
+                    println!("[GET_BIDASK] err");
+                    result = HashMap::new();
                 }
             };
-        } else {}
+        } else {
+            result = HashMap::new();
+        }
+        result
+    }
+
+    pub fn fetch_price(broker: &String) -> HashMap<String, Data> {
+        let url = get_url("price".to_string(), &broker);
+
+        let mut result: HashMap<String, Data>;
+        if let Ok(mut res) = reqwest::get(&url) {
+            let getres = match res.text() {
+                Ok(val) => {
+
+                    let v = getGeneric_hashmap("price".to_string(), broker.to_string(), val);
+
+                    result = v;
+                }
+                Err(err) => {
+                    println!("[GET_PRICE] err");
+                    result = HashMap::new();
+                }
+            };
+        } else {
+            result = HashMap::new();
+        }
         result
     }
 
@@ -230,7 +348,7 @@ mod Universal {
             } else if broker == "hitbtc" {
                 r = "https://api.hitbtc.com/api/2/public/ticker".to_string();
             }
-        }else if task == "price" {
+        } else if task == "price" {
             if broker == "binance" {
                 r = "https://api.binance.com/api/v3/ticker/price".to_string();
             } else if broker == "hitbtc" {
@@ -241,90 +359,10 @@ mod Universal {
     }
 }
 
-/*fn updateTicker(broker: String,mut ticker:&mut HashMap<String,String>) {
-    let br=broker.clone();
-    let data:String=Universal::fetch_ticker(broker);
-    ticker[&br]  =data;
-
-}*/
-
 fn handler_simple(_req: &mut Request) -> IronResult<Response> {
     Ok(Response::with((status::Ok, "Up")))
 }
 
 fn handler_favicon(_req: &mut Request) -> IronResult<Response> {
     Ok(Response::with((status::Ok, "Favicon")))
-}
-
-fn get_file_url(base_url: &str, broker: &str, pair: &str, interval: &str) -> String {
-    let mut s = base_url.to_string().to_owned();
-    s.push_str(broker);
-    s.push_str("-");
-    s.push_str(interval);
-    s.push_str("-");
-    s.push_str(pair);
-    s.push_str(".csv");
-    s
-}
-
-fn handler_query_bars(req: &mut Request) -> IronResult<Response> {
-//let dt = std::time::Instant::now();
-    let ref broker: &str = req.extensions.get::<Router>().unwrap().find("broker").unwrap_or("/");
-    let url = &req.url;
-    /*match CACHE.get(url) {
-        Some(&res) => {
-            Ok(Response::with((status::Ok, res)))
-        }
-        None => {*/
-    if broker.len() > 1 {
-        let ref interval = req.extensions.get::<Router>().unwrap().find("interval").unwrap_or("/");
-        let ref pair = req.extensions.get::<Router>().unwrap().find("pair").unwrap_or("/");
-        let ref limit = req.extensions.get::<Router>().unwrap().find("limit").unwrap_or("/");
-        let n_results: usize = limit.parse::<usize>().unwrap();
-        let filename = get_file_url("../websockets/data/", broker, pair, interval);
-        println!("File {}", filename);
-        let mut f = File::open(filename).expect("file not found");
-        let mut contents = String::new();
-        f.read_to_string(&mut contents).expect("something went wrong reading the file");
-        let split = contents.split("\n");
-        let splitcol: Vec<&str> = split.collect();
-//  let dt0 = dt.elapsed();
-//let NN=split.count();
-        let N: usize = splitcol.len();
-        println!("lines {}", N);
-        let mut res: String = "{\"error\":\"false\",\"success\":\"true\",\"data\":[".to_string();
-
-        let mut first = true;
-        let mut current_line = 0;
-        let min: usize = 0;
-        let begin = std::cmp::max(min, N - n_results);
-        for s in splitcol.iter() {
-            if (current_line > begin) {
-                let line: Vec<&str> = s.split(",").collect();
-                if (line.len() > 1) {
-                    if (!first) { res.push_str(","); }
-                    res.push_str("{\"ts\":\"");
-                    res.push_str(line[0]);
-                    res.push_str("\",\"o\":\"");
-                    res.push_str(line[1]);
-                    res.push_str("\",\"h\":\"");
-                    res.push_str(line[2]);
-                    res.push_str("\",\"l\":\"");
-                    res.push_str(line[3]);
-                    res.push_str("\",\"c\":\"");
-                    res.push_str(line[4]);
-                    res.push_str("\",\"v\":\"");
-                    res.push_str(line[5]);
-                    res.push_str("\"}");
-                    first = false;
-                }
-            }
-            current_line = current_line + 1;
-        }
-        res.push_str("] }");
-        println!("Cache {}", url.to_string());
-        Ok(Response::with((status::Ok, res)))
-    } else {
-        Ok(Response::with((status::Ok)))
-    }
 }
